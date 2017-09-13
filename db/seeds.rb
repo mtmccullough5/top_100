8010fd4f28e45587cc4329a4f594895210fa0789
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-09-12 20:01:22 -0600

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+genres = ["Rock", "Alternative", "Country", "Pop", "Electronic", "Blues"]
+100.times do
+  Song.create(
+    title: Faker::Book.unique.title
+    artist: Faker::RockBand.name
+    rank: [1..100].sample,
+    genre: genres.sample
+  )
+end
+
+puts "data has been seeded"
